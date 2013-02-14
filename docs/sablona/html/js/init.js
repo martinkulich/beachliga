47f7dfa9e41c33b9aa680bc026735e516d94804a
Martin Kulich <martin.kulich@imatic.cz>
2013-02-14 09:57:18 +0100

    podklady

---
+++ docs/sablona/html/js/init.js
@@ -0,0 +1,13 @@
+_5grid.ready(function() {
+
+	if (_5grid.isDesktop)
+	{
+		$('#nav > ul').dropotron({ 
+			offsetY: -16,
+			mode: 'fade',
+			noOpenerFade: true,
+			hideDelay: 400
+		});
+	}
+
+});
