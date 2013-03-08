9d3af445ea0d60c97e0d57da30d138435772dd77
Martin Kulich <martin.kulich@imatic.cz>
2013-03-08 11:35:22 +0100

    bannery

---
+++ system/modules/banner/config/config.php
@@ -0,0 +1,88 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+/**
+ * Contao Open Source CMS, Copyright (C) 2005-2013 Leo Feyer
+ * 
+ * Modul Banner Config - Backend
+ *
+ * This is the banner configuration file.
+ *
+ * @copyright	Glen Langer 2007..2013 <http://www.contao.glen-langer.de>
+ * @author      Glen Langer (BugBuster)
+ * @package     Banner 
+ * @license     GPL 
+ * @filesource
+ */
+
+
+/**
+ * -------------------------------------------------------------------------
+ * BACK END MODULES
+ * -------------------------------------------------------------------------
+ *
+ * Back end modules are stored in a global array called "BE_MOD". Each module 
+ * has certain properties like an icon, an optional callback function and one 
+ * or more tables. Each module belongs to a particular group.
+ * 
+ *   $GLOBALS['BE_MOD'] = array
+ *   (
+ *       'group_1' => array
+ *       (
+ *           'module_1' => array
+ *           (
+ *               'tables'       => array('table_1', 'table_2'),
+ *               'key'          => array('Class', 'method'),
+ *               'callback'     => 'ClassName',
+ *               'icon'         => 'path/to/icon.gif',
+ *               'stylesheet'   => 'path/to/stylesheet.css'
+ *               'javascript'   => 'path/to/javascript.js'
+ *           )
+ *       )
+ *   );
+ * 
+ * Use function array_insert() to modify an existing modules array.
+ */
+$GLOBALS['BE_MOD']['content']['banner'] = array
+(
+	'tables'     => array('tl_banner_category', 'tl_banner'),
+	//'icon'       => ModuleBannerFile::BannerIcon('iconBanner.gif'),
+	'icon'       => 'system/modules/banner/iconBanner.gif',
+	//'stylesheet' => ModuleBannerFile::BannerCss('mod_banner_be.css')
+	'stylesheet' => 'system/modules/banner/mod_banner_be.css'
+);
+
+array_insert($GLOBALS['BE_MOD']['system'], 0, array
+(
+	'bannerstat' => array
+	(
+		'callback'   => 'ModuleBannerStat',
+		//'icon'       => ModuleBannerFile::BannerIcon('iconBannerStat.gif'),
+		'icon'       => 'system/modules/banner/iconBannerStat.gif',
+		//'stylesheet' => ModuleBannerFile::BannerCss('mod_banner_be.css')
+		'stylesheet' => 'system/modules/banner/mod_banner_be.css'
+	)
+));
+
+/**
+ * -------------------------------------------------------------------------
+ * FRONT END MODULES
+ * -------------------------------------------------------------------------
+ *
+ * List all fontend modules and their class names.
+ * 
+ *   $GLOBALS['FE_MOD'] = array
+ *   (
+ *       'group_1' => array
+ *       (
+ *           'module_1' => 'Contentlass',
+ *           'module_2' => 'Contentlass'
+ *       )
+ *   );
+ * 
+ * Use function array_insert() to modify an existing CTE array.
+ */
+array_insert($GLOBALS['FE_MOD']['miscellaneous'], 0, array
+(
+	'banner' => 'ModuleBanner')
+);
+
+?>
---
+++ system/modules/banner/languages/en/tl_module.php
@@ -0,0 +1,29 @@
+<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+/**
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2010 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
+ * 
+ * Modul Banner Sprachdateien
+ * 
+ * Language file for table tl_module.
+ *
+ * PHP version 5
+ * @copyright  Glen Langer 2007..2010
+ * @author     Glen Langer
+ * @package    BannerLanguage 
+ * @license    GPL 
+ * @filesource  
+ */
+
+/**
+ * Fields
+ */
+$GLOBALS['TL_LANG']['tl_module']['banner_hideempty']  = array('Hide when empty', 'Hide the module (Frontend) when no active banners are found.');
+$GLOBALS['TL_LANG']['tl_module']['banner_firstview']  = array('First banner for new visitor', 'For each new visitor, at first is displayed the first banner in the category. The choice is then returned to random mode.');
+$GLOBALS['TL_LANG']['tl_module']['banner_categories'] = array('Banner categories', 'Please select the Banner category.');
+$GLOBALS['TL_LANG']['tl_module']['banner_template']   = array('Banner template', 'Please choose the Banner-Template. Banner template files start with <em>mod_banner_list_</em>.'); 
+$GLOBALS['TL_LANG']['tl_module']['banner_useragent']  = array('No counting for this HTTP_USER_AGENT partial identifier', 'Here you can specify a partial identifier, it will be no counting. These HTTP_USER_AGENT partial identifier of the browser must be changed.<br />See Wiki.');
+ 
+?>
